84b5f77658430ffac94e8a70f32bf30994dca92e
bigdeng <ok@bigdeng.com>
2022-07-29 11:22:43 +0800

    首次提交

---
+++ test/random.test.js
@@ -0,0 +1,20 @@
+describe('Random API:', function () {
+    describe('#randomColor()', function () {
+        it(`/^#[0-9a-fA-F]$/.test(outils.randomColor()) should return true`, function () {
+            assert(/^#[0-9a-fA-F]{6}$/.test(outils.randomColor()))
+        });
+    });
+
+    describe('#randomNum()', function () {
+        it(`10 <= outils.randomNum(10, 1000) <= 100 should return true`, function () {
+            let num = outils.randomNum(10, 1000)
+            assert(num <= 1000 && num > 10)
+        });
+
+        // 测试 0.1 ~ 1.2 之间只能取得 1，不能为 0
+        it(`1 === outils.randomNum(0.1, 1.2) should return true`, function () {
+            let num = outils.randomNum(0.1, 1.2)
+            assert(num === 1)
+        });
+    });
+})
